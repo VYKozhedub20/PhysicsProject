d4f337d9cd27f73a17072cbe462fcb7c8ca6f836
Viktor Kozhedub <85335552+VYKozhedub20@users.noreply.github.com>
2021-12-05 21:08:01 +0000

    Added a few small additions

--- main.cpp
+++ main.cpp
@@ -24,10 +24,12 @@ int main()
       if(liquid[i] == 'h' && liquid[i + 1] == 'o' && liquid[i + 2] == 'n' && liquid[i + 3] == 'e' &&liquid[i + 4] == 'y')
       {
         rho = 1.42;
+        cout << "The density of honey is 1 gram per milliliter." << endl;
       }
       else if(liquid[i] == 'w' && liquid[i + 1] == 'a' && liquid[i + 2] == 't' && liquid[i + 3] == 'e' &&liquid[i + 4] == 'r')
       {
         rho = 1;
+        cout << "The density of water is 1 gram per milliliter." << endl;
       }
     }
     if(LiquidLenght == 7)
@@ -35,6 +37,7 @@ int main()
       if(liquid[i] == 'a' && liquid[i + 1] == 'l' && liquid[i + 2] == 'c' && liquid[i + 3] == 'o' &&liquid[i + 4] == 'h' && liquid[i + 5] == 'o' && liquid[i + 6] == 'l')
       {
         rho = 0.8;
+        cout << "The density of alcohol is 1 gram per milliliter." << endl;
       }
     }
   }
@@ -52,6 +55,7 @@ int main()
       if(planet[i] == 'M' && planet[i + 1] == 'a' && planet[i + 2] == 'r' && planet[i  + 3] == 's')
       {
         g = 3.7;
+        cout << "The free-fall acceleration on the surface of Mars is 3.7 meters per square second." << endl;
       }
     }
     if(PlanetLenght == 5)
@@ -59,10 +63,12 @@ int main()
       if(planet[i] == 'V' && planet[i + 1] == 'e' && planet[i + 2] == 'n' && planet[i  + 3] == 'u' && planet[i  + 4] == 's')
       {
         g = 8.9;
+        cout << "The free-fall acceleration on the surface of Venus is 8.9 meters per square second." << endl;
       }
       else if(planet[i] == 'E' && planet[i + 1] == 'a' && planet[i + 2] == 'r' && planet[i  + 3] == 't' && planet[i  + 4] == 'h')
       {
         g = 9.8;
+        cout << "The free-fall acceleration on the surface of Earth is 9.8 meters per square second." << endl;
       }
     }
     if(PlanetLenght == 6)
@@ -70,10 +76,12 @@ int main()
       if(planet[i] == 'S' && planet[i + 1] == 'a' && planet[i + 2] == 't' && planet[i  + 3] == 'u' && planet[i  + 4] == 'r' && planet[i  + 5] == 'n')
       {
         g = 10.4;
+        cout << "The free-fall acceleration on the surface of Saturn is 10.4 meters per square second." << endl;
       }
       else if(planet[i] == 'U' && planet[i + 1] == 'r' && planet[i + 2] == 'a' && planet[i  + 3] == 'n' && planet[i  + 4] == 'u' && planet[i  + 5] == 's')
       {
         g = 8.9;
+        cout << "The free-fall acceleration on the surface of Uranus is 8.9 meters per square second." << endl;
       }
     }
     if(PlanetLenght == 7)
@@ -82,12 +90,14 @@ int main()
       && planet[i  + 6] == 'r')
       {
         g = 24.8;
+        cout << "The free-fall acceleration on the surface of Jupiter is 24.8 meters per square second." << endl;
       }
-    }
-    else if(planet[i] == 'N' && planet[i + 1] == 'e' && planet[i + 2] == 'p' && planet[i  + 3] == 't' && planet[i  + 4] == 'u' && planet[i  + 5] == 'n'
-    && planet[i  + 6] == 'e')
-    {
-      g = 11;
+      else if(planet[i] == 'N' && planet[i + 1] == 'e' && planet[i + 2] == 'p' && planet[i  + 3] == 't' && planet[i  + 4] == 'u' && planet[i  + 5] == 'n'
+      && planet[i  + 6] == 'e')
+      {
+        g = 11;
+        cout << "The free-fall acceleration on the surface of Neptune is 11 meters per square second." << endl;
+      }
     }
   }
   cout << "Depth(in meters): ";
