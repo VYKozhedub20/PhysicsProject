f75ab3a1eb5cb403fe023c5b6c5235174d591741
Viktor Kozhedub <85335552+VYKozhedub20@users.noreply.github.com>
2021-12-05 13:41:29 +0000

    added honey

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 int main() 
 {
-  char liquid;
-  char object;
+  char liquid[100];
+  char Planet[100];
+  int LiquidLenght;
+  int PlanetLenght;
+  int g, rho;
+
+  cout << "Liquid: ";
+
+  cin.getline(liquid, 100, '\n');
+
+  LiquidLenght = strlen(liquid);
+
+  for(int i = 0; i < LiquidLenght; i++)
+  {
+    if(LiquidLenght == 5)
+    {
+      if(liquid[i] == 'h' && liquid[i + 1] == 'o' && liquid[i + 2] == 'n' && liquid[i + 3] == 'e' &&liquid[i + 4] == 'y')
+      {
+        rho = 1;
+      }
+    }
+  }
 } 
